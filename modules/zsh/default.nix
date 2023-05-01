89332def1e66014a7fcf1dcf8e7514c840ccad8d
m-macdonald <37754975+m-macdonald@users.noreply.github.com>
2023-05-01 16:32:12 -0400

    Changed zsh config to allow for more freedom in customization

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -5,19 +5,28 @@ in {
   options.modules.zsh = { enable = mkEnableOption "zsh"; };
 
   config = mkIf cfg.enable {
-    # Sets .zshrc to my root home directory.
-    # TODO: Maybe find a way to move this into the .config/zsh folder?
-    home.file.".zshrc".source = ./config/.zshrc;
-
-    # Sets the config files for the zim plugin manager
-    home.file."zim" = {
-      source = ./config/zim;
-      target = ".config/zsh/zim";
-      recursive = true;
+    home = {
+      packages = with pkgs; [ zsh ];   
+      file = {
+        # Sets .zshrc to my root home directory.
+        # TODO: Maybe find a way to move this into the .config/zsh folder?
+        ".zshrc".source = ./config/.zshrc;
+        # Sets the config files for the zim plugin manager
+        "zim" = {
+          source = ./config/zim;
+          target = ".config/zsh/zim";
+          recursive = true;
+        };
+      };
     };
+  };
 
-    # Additional zsh config
+  # Can be used if you want to keep the config within nix. 
+  # I prefer doing as above and allowing the config to be a separate dedicated file.
+  # Provides more freedom
+/*
     programs.zsh = {
+      dotDir = ".config/zsh";
       enableCompletion = true;
       enableSyntaxHighlighting = true;
       sessionVariables = {
@@ -31,4 +40,5 @@ in {
       };
     };
   };
+  */
 }
