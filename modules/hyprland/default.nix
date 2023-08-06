b6e88cc45d61ad679653aaeb618ed359d2c0f70a
m-macdonald <37754975+m-macdonald@users.noreply.github.com>
2023-08-05 20:22:21 -0400

    Updated Hyprland config to align with new version

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -2,7 +2,16 @@
 with lib;
 let cfg = config.modules.hyprland;
   swww-init = pkgs.writeShellScriptBin "swww-init" ''
-    swww init
+    #! /usr/bin/env bash
+    files=(/mnt/nas/media/images/pixiv/*)
+
+    random_background=''${files[RANDOM % ''${#files[@]}]}
+
+    if ! ps -ef | grep -v grep | grep -q "swww-daemon"; then
+      swww init
+    fi
+    echo ''$random_background
+    swww img "$random_background"
   '';
 in {
   options.modules.hyprland = { enable = mkEnableOption "hyprland"; };
