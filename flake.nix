b87b201a72611a1a65ebe526f2087024008384e2
m-macdonald <37754975+m-macdonald@users.noreply.github.com>
2025-03-23 15:56:42 -0400

    Added media-center host public key to sops config

--- flake.nix
+++ flake.nix
@@ -111,9 +111,8 @@
                             sops = {
                                 defaultSopsFile = ./secrets/secrets.yaml;
                                 age = {
-                                    sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
                                     keyFile = "/var/lib/sops-nix/key.txt";
-                                    generateKey = true;
+                                    generateKey = false;
                                 };
                                 secrets = {
                                     "users/maddux/password" = {
