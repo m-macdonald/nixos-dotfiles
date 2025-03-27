6fec65e3c482ffeadc7662c979bf1c1d9ff852bf
m-macdonald <37754975+m-macdonald@users.noreply.github.com>
2025-03-27 00:46:22 +0000

    cifs mount is working now. Not really sure what did it, so I'm committing it all as is.

--- modules/nixos/network-shares/default.nix
+++ modules/nixos/network-shares/default.nix
@@ -1,6 +1,19 @@
-{  pkgs, config, ... }:
+{  pkgs, config, lib, ... }:
 let
-    options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=300" "x-systemd.device-timeout=5s" "x-systemd.mount-timeout=30s" "uid=nobody" "gid=users" "credentials=/run/secrets/nas" ];
+    options = [ 
+        "x-systemd.automount"
+        "noauto" 
+        "x-systemd.idle-timeout=300"
+        "x-systemd.device-timeout=5s"
+        "x-systemd.mount-timeout=30s"
+        "credentials=/run/secrets/nas"
+        "rw"
+        "uid=${toString config.users.users.maddux.uid}" 
+        "gid=${toString config.users.groups.users.gid}"
+        "file_mode=0777"
+        "dir_mode=0777"
+        "noperm"
+    ];
 in
 {
     config = {
@@ -9,16 +22,24 @@ in
         sops.secrets.nas = {};
 
         fileSystems = {
-            "/mnt/nas/appdata" = {
-                device = "//tower/appdata";
-                fsType = "cifs";
-                options = options;
-            };
+            # "/mnt/nas/appdata" = {
+            #     device = "//tower/appdata";
+            #     fsType = "cifs";
+            #     options = options;
+            # };
             "/mnt/nas/media" = {
-                device = "//tower/Media";
+                device = "//tower/media";
                 fsType = "cifs";
                 options = options;
             };
+        };
+
+        security.wrappers."mount.cifs" = {
+            program = "mount.cifs";
+            source = "${lib.getBin pkgs.cifs-utils}/bin/mount.cifs";
+            owner = "root";
+            group = "root";
+            setuid = true;
         };
     };
 }
