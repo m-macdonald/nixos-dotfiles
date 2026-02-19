d1ce63951f0452379b29a0fcf8f58d6c4a757fe2
m-macdonald <37754975+m-macdonald@users.noreply.github.com>
2026-02-18 21:34:41 -0500

    Alphabetized inputs

--- flake.nix
+++ flake.nix
@@ -2,43 +2,43 @@
   description = "My Nixos Configurations";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-25.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    nur = {
-      url = "github:nix-community/NUR";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nixos-hardware.url = "github:NixOS/nixos-hardware";
-    xdph.url = "github:hyprwm/xdg-desktop-portal-hyprland";
-    nixvim = {
-      url = "github:m-macdonald/nixvim-flake";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
-    };
-    sops-nix = {
-      url = "github:Mic92/sops-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    play-nix = {
-      url = "github:TophC7/play.nix/fea2ccf";
-    };
     chaotic = {
       url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
+    dw-proton.url = "github:imaviso/dwproton-flake";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     niri = {
       url = "github:sodiboo/niri-flake";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
-    dw-proton.url = "github:imaviso/dwproton-flake";
+    nixos-hardware.url = "github:NixOS/nixos-hardware";
+    nixpkgs.url = "nixpkgs/nixos-25.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixvim = {
+      url = "github:m-macdonald/nixvim-flake";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
     noctalia = {
       url = "github:noctalia-dev/noctalia-shell";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
+    nur = {
+      url = "github:nix-community/NUR";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    play-nix = {
+      url = "github:TophC7/play.nix/fea2ccf";
+    };
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    xdph.url = "github:hyprwm/xdg-desktop-portal-hyprland";
   };
 
   outputs = inputs:
